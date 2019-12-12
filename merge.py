35da0513b033adf9e0fcc1a5718ed38267bfd811
kuderr <dakudryavcev@gmail.com>
2019-12-12 18:08:57 +0300

    lock context manager

--- merge.py
+++ merge.py
@@ -11,59 +11,58 @@ NVR_API_KEY = 'ce72d95a264248558f352768d620ca16'
 
 
 def merge_video(client_url: str, screen_num: str, cam_num: str, record_name: str, room_id: int, calendar_id: str, event_id: str) -> None:
-    lock.acquire()
+    with lock:
 
-    first = subprocess.Popen(["ffmpeg", "-i", home + "/vids/vid_" + cam_num + ".mp4", "-i", home + "/vids/vid_" +
-                              screen_num + ".mp4", "-filter_complex", "hstack=inputs=2", home + "/vids/vid_" +
-                              record_name + "merged.mp4"], shell=False)
-    os.system("renice -n 20 %s" % (first.pid, ))
-    first.wait()
+        first = subprocess.Popen(["ffmpeg", "-i", home + "/vids/vid_" + cam_num + ".mp4", "-i", home + "/vids/vid_" +
+                                  screen_num + ".mp4", "-filter_complex", "hstack=inputs=2", home + "/vids/vid_" +
+                                  record_name + "merged.mp4"], shell=False)
+        os.system("renice -n 20 %s" % (first.pid, ))
+        first.wait()
 
-#    mid1 = subprocess.Popen(
-#         ["ffmpeg", "-i", home + "/vids/vid_" + screen_num + ".mp4", "-s", "hd720",
-#          home + "/vids/" + record_name + "mid_screen.mp4"], shell=False)
-#    os.system("renice -n 20 %s" % (mid1.pid,))
-#    mid1.wait()
+    #    mid1 = subprocess.Popen(
+    #         ["ffmpeg", "-i", home + "/vids/vid_" + screen_num + ".mp4", "-s", "hd720",
+    #          home + "/vids/" + record_name + "mid_screen.mp4"], shell=False)
+    #    os.system("renice -n 20 %s" % (mid1.pid,))
+    #    mid1.wait()
 
-#    mid2 = subprocess.Popen(
-#         ["ffmpeg", "-i", home + "/vids/vid_" + cam_num + ".mp4", "-s", "hd720",
-#          home + "/vids/" + record_name + "mid_cam.mp4"], shell=False)
-#    os.system("renice -n 20 %s" % (mid2.pid,))
-#    mid2.wait()
+    #    mid2 = subprocess.Popen(
+    #         ["ffmpeg", "-i", home + "/vids/vid_" + cam_num + ".mp4", "-s", "hd720",
+    #          home + "/vids/" + record_name + "mid_cam.mp4"], shell=False)
+    #    os.system("renice -n 20 %s" % (mid2.pid,))
+    #    mid2.wait()
 
-#    crop1 = subprocess.Popen(
-#         ["ffmpeg", "-i", home + "/vids/" + record_name + "mid_cam.mp4", "-filter:v", "crop=640:720:40:0",
-#          home + "/vids/" + record_name + "cropped_cam.mp4"], shell=False)
+    #    crop1 = subprocess.Popen(
+    #         ["ffmpeg", "-i", home + "/vids/" + record_name + "mid_cam.mp4", "-filter:v", "crop=640:720:40:0",
+    #          home + "/vids/" + record_name + "cropped_cam.mp4"], shell=False)
 
-#    os.system("renice -n 20 %s" % (crop1.pid,))
-#    crop1.wait()
+    #    os.system("renice -n 20 %s" % (crop1.pid,))
+    #    crop1.wait()
 
-#    second = subprocess.Popen(
-#         ["ffmpeg", "-i", home + "/vids/" + record_name + "cropped_cam.mp4", "-i", home + "/vids/" +
-#          record_name + "mid_screen.mp4", "-filter_complex", "hstack=inputs=2", home + "/vids/vid_" +
-#          record_name + "merged.mp4"], shell=False)
-#    os.system("renice -n 20 %s" % (second.pid,))
-#    second.wait()
+    #    second = subprocess.Popen(
+    #         ["ffmpeg", "-i", home + "/vids/" + record_name + "cropped_cam.mp4", "-i", home + "/vids/" +
+    #          record_name + "mid_screen.mp4", "-filter_complex", "hstack=inputs=2", home + "/vids/vid_" +
+    #          record_name + "merged.mp4"], shell=False)
+    #    os.system("renice -n 20 %s" % (second.pid,))
+    #    second.wait()
 
-    res = ""
-    if os.path.exists(home + "/vids/sound_" + record_name + ".aac"):
-        add_sound(record_name +
-                  "merged", record_name)
-    else:
-        res = "vid_"
+        res = ""
+        if os.path.exists(home + "/vids/sound_" + record_name + ".aac"):
+            add_sound(record_name +
+                      "merged", record_name)
+        else:
+            res = "vid_"
 
-    requests.post(client_url + "/upload-merged",
-                  json={
-                      "file_name": res + record_name + "merged.mp4",
-                      "room_id": room_id,
-                      "calendar_id": calendar_id,
-                      "event_id": event_id
-                  },
-                  headers={'content-type': 'application/json',
-                           "key": NVR_API_KEY}
-                  )
-
-    lock.release()
+        res = requests.post(client_url + "/upload-merged",
+                            json={
+                                "file_name": res + record_name + "merged.mp4",
+                                "room_id": room_id,
+                                "calendar_id": calendar_id,
+                                "event_id": event_id
+                            },
+                            headers={'content-type': 'application/json',
+                                     "key": NVR_API_KEY}
+                            )
+        print(res.json())
 
 
 def add_sound(record_name: str, audio_cam_num: str) -> None:
