1e51aa7d82cf6317210ab6c56ef8fc9fbf8f8f22
burnouttt <burnouttt@yandex.ru>
2020-03-24 18:10:23 +0300

    Fixes

--- merge.py
+++ merge.py
@@ -125,8 +125,7 @@ def create_merge(cameras_file_name: str, screens_file_name: str,
 
         hours = f'{duration // 60}' if (duration //
                                         60) > 9 else f'0{duration // 60}'
-        minutes = f'{duration % 60}' if (
-                                                duration % 60) > 9 else f'0{duration % 60}'
+        minutes = f'{duration % 60}' if (duration % 60) > 9 else f'0{duration % 60}'
         vid_dur = f'{hours}:{minutes}:00'
         vid_start = f'00:{time_to_cut_1}:00' if time_to_cut_1 > 9 else f'00:0{time_to_cut_1}:00'
         cam_cutting = subprocess.Popen(['ffmpeg', '-ss', vid_start, '-t', vid_dur, '-i',
