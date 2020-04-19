99f77b7e77a5fbbb1831cdafa57ad38e3664f165
Burnouttt <burnouttt@yandex.ru>
2020-04-19 18:20:52 +0300

    New course fetch mechanism + google spreadsheets api

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 import time
-from datetime import datetime, timedelta
 import traceback
+from datetime import datetime, timedelta
+
 import requests
 import schedule
 
+from calendar_api import add_attachments
+from classroom_api import create_assignment
 from driveAPI import get_folders_by_name, share_file
-from calendarAPI import add_attachments
-from classroom_api import create_assignment, get_course_by_code
 from merge import get_files, create_merge
 from models import Session, Record, Room
+from spreadsheets_api import get_data
 
 
 class DaemonApp:
+    class_sheet_id = '1_YP63y3URvKCMXjHJC7neOJRk1Uyj6DEJTL0jkLYOEI'
+    class_sheet_range = 'A2:B500'
 
     def __init__(self):
         schedule.every().hour.at(":00").do(self.invoke_merge_events)
@@ -19,18 +23,17 @@ class DaemonApp:
 
     def invoke_merge_events(self):
         session = Session()
-        process_record = session.query(Record).filter(
-            Record.processing == True).first()
+        process_record = session.query(Record).filter(Record.processing is True).first()
 
         if process_record:
             session.close()
             return
 
-        records_to_create = session.query(Record).filter(Record.done == False,
-                                                         Record.processing == False).all()
+        records_to_create = session.query(Record).filter(Record.done is False,
+                                                         Record.processing is False).all()
 
+        now_moscow = datetime.now() + timedelta(hours=3)
         try:
-            now_moscow = datetime.now() + timedelta(hours=3)
             record = next(record for record in records_to_create
                           if now_moscow >= datetime.strptime(f'{record.date} {record.end_time}', '%Y-%m-%d %H:%M'))
         except StopIteration:
@@ -58,24 +61,28 @@ class DaemonApp:
 
             share_file(file_id, record.user_email)
             share_file(backup_file_id, record.user_email)
-            self.send_zulip_msg(
-                record.user_email, f'Ваша склейка в NVR готова: https://drive.google.com/a/auditory.ru/file/d/{file_id}/view?usp=drive_web')
+            self.send_zulip_msg(record.user_email,
+                                f'Ваша склейка в NVR готова: '
+                                f'https://drive.google.com/a/auditory.ru/file/d/{file_id}/view?usp=drive_web')
 
             if calendar_id:
                 try:
                     file_ids = [file_id, backup_file_id]
                     file_urls = [
-                        f"\nhttps://drive.google.com/a/auditory.ru/file/d/{file_id}/view?usp=drive_web" for file_id in file_ids]
+                        f"\nhttps://drive.google.com/a/auditory.ru/file/d/{file_id}/view?usp=drive_web" for file_id in
+                        file_ids]
 
                     description = add_attachments(calendar_id,
                                                   record.event_id,
                                                   file_urls)
 
                     course_code = description.split('\n')[0]
-                    course = get_course_by_code(course_code)
-                    if course:
-                        create_assignment(course.get('id', ''),
-                                          record.event_name, file_urls)
+                    courses = get_data(self.class_sheet_id, self.class_sheet_range)
+
+                    course_id = self.get_course_by_code(course_code, courses)
+
+                    if course_id:
+                        create_assignment(course_id, record.event_name, file_urls)
                 except:
                     traceback.print_exc()
 
@@ -87,7 +94,7 @@ class DaemonApp:
             session.commit()
             session.close()
 
-    def get_folder_id(self, date: str, room: Room):
+    def get_folder_id(self, date: str, room: Room) -> str:
         folders = get_folders_by_name(date)
 
         for folder_id, folder_parent_id in folders.items():
@@ -97,6 +104,13 @@ class DaemonApp:
             folder_id = room.drive.split('/')[-1]
 
         return folder_id
+
+    def get_course_by_code(self, course_code: str, courses: list) -> str:
+        for course in courses:
+            if course[0].strip() == course_code:
+                return course[1].strip()
+
+        raise RuntimeError(f"Курс с предметной единицей '{course_code}' не найден")
 
     def send_zulip_msg(self, email, msg):
         res = requests.post('http://172.18.130.41:8080/api/send-msg', json={
