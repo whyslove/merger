5e73a0f4489df29d5068b5fb2ae4cacb8e9c19de
kuderr <dakudryavcev@gmail.com>
2020-05-04 15:18:48 +0300

    nvr env get

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 docker stop nvr_merger
 docker rm nvr_merger
 docker build -t nvr_merger .
-docker run -idt --name nvr_merger --net=host --env-file .env nvr_merger
+docker run -idt --name nvr_merger --net=host --env-file ../.env_nvr nvr_merger
