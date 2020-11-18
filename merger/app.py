ae4d6c69b76f4940c9beaf7a8ae826fce9396d1f
kuderr <dakudryavcev@gmail.com>
2020-11-18 22:49:53 +0300

    changes in models and main acording to

--- merger/app.py
+++ merger/app.py
@@ -12,7 +12,12 @@ import schedule
 
 from core.apis.calendar_api import add_attachments, calendar_refresh_token
 from core.apis.classroom_api import create_announcement, classroom_refresh_token
-from core.apis.driveAPI import get_folders_by_name, share_file, upload_video, drive_refresh_token
+from core.apis.driveAPI import (
+    get_folders_by_name,
+    share_file,
+    upload_video,
+    drive_refresh_token,
+)
 from core.apis.spreadsheets_api import get_data, sheets_refresh_token
 from core.db.models import Session, Record, Room
 from core.db.utils import update_record_driveurl
@@ -23,47 +28,57 @@ HOME = str(Path.home())
 
 
 class DaemonApp:
-    class_sheet_id = '1nyQ0M_3RozJ-MpTuji7eIbsxR7pk1wNPzVgCaiNMryU'
-    class_sheet_range = 'A2:B1000'
-    logger = logging.getLogger('merger_logger')
+    class_sheet_id = "1nyQ0M_3RozJ-MpTuji7eIbsxR7pk1wNPzVgCaiNMryU"
+    class_sheet_range = "A2:B1000"
+    logger = logging.getLogger("merger_logger")
 
     def __init__(self):
-        self.logger.info('Class \"DaemonApp\" instantiated')
+        self.logger.info('Class "DaemonApp" instantiated')
         schedule.every(10).minutes.do(self.invoke_merge_events)
 
     def invoke_merge_events(self):
-        self.logger.info(f'Starting merge check')
+        self.logger.info(f"Starting merge check")
 
         session = Session()
-        process_record = session.query(Record).filter(
-            Record.processing == True).first()
+        process_record = session.query(Record).filter(Record.processing == True).first()
 
         if process_record:
             self.logger.info(
-                f'Found processing merge with id {process_record.id}, skipping iteration')
+                f"Found processing merge with id {process_record.id}, skipping iteration"
+            )
             session.close()
             return
 
-        records_to_create = session.query(Record).filter(Record.done == False,
-                                                         Record.processing == False,
-                                                         Record.error == False).order_by(Record.id).all()
+        records_to_create = (
+            session.query(Record)
+            .filter(
+                Record.done == False, Record.processing == False, Record.error == False
+            )
+            .order_by(Record.id)
+            .all()
+        )
 
         try:
-            record = next(record for record in records_to_create
-                          if datetime.now() >= self.planned_drive_upload(record))
+            record = next(
+                record
+                for record in records_to_create
+                if datetime.now() >= self.planned_drive_upload(record)
+            )
             initially_error = False
         except StopIteration:
-            self.logger.info(f'Records not found at {datetime.now()}')
-            records = session.query(Record).filter(
-                Record.error == True, Record.done == False).all()
+            self.logger.info(f"Records not found at {datetime.now()}")
+            records = (
+                session.query(Record)
+                .filter(Record.error == True, Record.done == False)
+                .all()
+            )
             if not records:
                 session.close()
                 return
 
             for record in records:
                 if datetime.now() >= self.planned_drive_upload(record, delta=180):
-                    self.logger.info(
-                        f'Restart record with error: {record.event_name}')
+                    self.logger.info(f"Restart record with error: {record.event_name}")
                     initially_error = True
                     record.error = False
                     break
@@ -73,10 +88,9 @@ class DaemonApp:
 
         try:
             self.logger.info(
-                f'Started merging record {record.event_name} with id {record.id}')
-            room = session.query(Room).filter(
-                Room.name == record.room_name).first()
-
+                f"Started merging record {record.event_name} with id {record.id}"
+            )
+            room = record.room
             record.processing = True
             session.commit()
 
@@ -86,27 +100,33 @@ class DaemonApp:
                 files = merge.create_merge()
             except RuntimeError:
                 self.logger.error(
-                    f'Source videos not found for record {record.event_name} with id {record.id} '
-                    f'or some error occured while merging')
+                    f"Source videos not found for record {record.event_name} with id {record.id} "
+                    f"or some error occured while merging"
+                )
 
                 asyncio.run(
-                    self.send_zulip_msg(record.user_email,
-                                        f'Некоторые исходные видео для вашей склейки "{record.event_name}" в NVR не были найдены на Google-диске, '
-                                        'и при подготовке склейки произошла ошибка')
+                    self.send_zulip_msg(
+                        record.users[0].email,
+                        f'Некоторые исходные видео для вашей склейки "{record.event_name}" в NVR не были найдены на Google-диске, '
+                        "и при подготовке склейки произошла ошибка",
+                    )
                 )
 
                 record.error = True
                 raise FilesNotFoundException(
-                    "Некоторые исходные видео не были найдены на Google-диске.")
+                    "Некоторые исходные видео не были найдены на Google-диске."
+                )
             except Exception as err:
                 record.error = True
                 raise err
             else:
-                Thread(target=asyncio.run, args=(self.apis_stuff(
-                    deepcopy(record), deepcopy(room), files), )).start()
+                Thread(
+                    target=asyncio.run,
+                    args=(self.apis_stuff(deepcopy(record), deepcopy(room), files),),
+                ).start()
 
         except Exception as err:
-            self.logger.error(f'Exception occurred: {err}')
+            self.logger.error(f"Exception occurred: {err}")
 
             if initially_error and record.error:  # second try to create merge failed
                 record.done = True
@@ -115,7 +135,8 @@ class DaemonApp:
 
             if not record.error:
                 self.logger.info(
-                    f'Setting record {record.event_name} with id {record.id} as done')
+                    f"Setting record {record.event_name} with id {record.id} as done"
+                )
                 record.done = True
 
             record.processing = False
@@ -125,7 +146,8 @@ class DaemonApp:
     def planned_drive_upload(self, record, delta=60):
         delta = timedelta(minutes=delta)
         record_end_time = datetime.strptime(
-            f'{record.date} {record.end_time}', '%Y-%m-%d %H:%M')
+            f"{record.date} {record.end_time}", "%Y-%m-%d %H:%M"
+        )
 
         if record_end_time.minute in [0, 30]:
             pass
@@ -139,14 +161,16 @@ class DaemonApp:
     # change to own NVR notifier
     async def send_zulip_msg(self, email, msg):
         async with ClientSession() as session:
-            res = await session.post('http://172.18.130.41:8080/api/send-msg', ssl=False,
-                                     json={
-                                         "email": email,
-                                         "msg": msg
-                                     })
+            res = await session.post(
+                "http://172.18.130.41:8080/api/send-msg",
+                ssl=False,
+                json={"email": email, "msg": msg},
+            )
 
-        self.logger.info(f'Request to Zulip bot returned code {res.status}. \
-            Email: {email}, Message: {msg}')
+        self.logger.info(
+            f"Request to Zulip bot returned code {res.status}. \
+            Email: {email}, Message: {msg}"
+        )
 
     async def apis_stuff(self, record, room, files):
         drive_refresh_token()
@@ -155,91 +179,99 @@ class DaemonApp:
 
         main_file_id = file_ids[0]
 
-        await update_record_driveurl(record, f'https://drive.google.com/file/d/{main_file_id}/preview')
+        await update_record_driveurl(
+            record, f"https://drive.google.com/file/d/{main_file_id}/preview"
+        )
 
-        self.logger.info(
-            'Merge was successfully processed, starting files sharing')
+        self.logger.info("Merge was successfully processed, starting files sharing")
 
-        await asyncio.gather(*[share_file(file_id, record.user_email)
-                               for file_id in file_ids]
-                             )
-        await self.send_zulip_msg(record.user_email,
-                                  f'Ваша склейка в NVR готова: '
-                                  f'https://drive.google.com/a/auditory.ru/file/d/{main_file_id}/view?usp=drive_web')
+        await asyncio.gather(
+            *[share_file(file_id, record.users[0].email) for file_id in file_ids]
+        )
+        await self.send_zulip_msg(
+            record.users[0].email,
+            f"Ваша склейка в NVR готова: "
+            f"https://drive.google.com/a/auditory.ru/file/d/{main_file_id}/view?usp=drive_web",
+        )
 
         calendar_id = room.calendar if record.event_id else None
         if not calendar_id:
             return
 
         self.logger.info(
-            f'Merge has calendar id {calendar_id}, starting creating attachments')
+            f"Merge has calendar id {calendar_id}, starting creating attachments"
+        )
 
         file_urls = [
             f"https://drive.google.com/a/auditory.ru/file/d/{file_id}/view?usp=drive_web"
-            for file_id in file_ids]
+            for file_id in file_ids
+        ]
 
         calendar_refresh_token()
-        description = await add_attachments(calendar_id,
-                                            record.event_id,
-                                            file_ids,
-                                            record.event_name)
+        description = await add_attachments(
+            calendar_id, record.event_id, file_ids, record.event_name
+        )
         desc_json = Merge.parse_description(description)
 
-        course_code = desc_json.get('поток')
+        course_code = desc_json.get("поток")
         if not course_code:
             self.logger.info(
-                f"Course code not provided for event {record.event_name} with id {record.event_id}")
+                f"Course code not provided for event {record.event_name} with id {record.event_id}"
+            )
             return
 
         sheets_refresh_token()
-        courses = await get_data(self.class_sheet_id,
-                                 self.class_sheet_range)
+        courses = await get_data(self.class_sheet_id, self.class_sheet_range)
 
         course_id = self.get_course_by_code(course_code, courses)
         if not course_id:
             return
 
         classroom_refresh_token()
-        await create_announcement(
-            course_id, record.event_name, file_ids, file_urls)
+        await create_announcement(course_id, record.event_name, file_ids, file_urls)
 
     async def get_folder_id(self, date: str, room: Room) -> str:
         self.logger.info(
-            f'Started getting folder id from date {date} and room {room.name}')
+            f"Started getting folder id from date {date} and room {room.name}"
+        )
 
         folders = await get_folders_by_name(date)
 
         # 2 unobvious tricks. Better contact a creator
-        room_drive_id = room.drive.split('/')[-1]
+        room_drive_id = room.drive.split("/")[-1]
         for folder_id, folder_parent_ids in folders.items():
             if room_drive_id in folder_parent_ids:
                 break
         else:
-            folder_id = room.drive.split('/')[-1]
+            folder_id = room.drive.split("/")[-1]
 
         return folder_id
 
     def get_course_by_code(self, course_code: str, courses: list) -> str or None:
         try:
             course_row = next(
-                course for course in courses if course and course[0].strip() == course_code)
+                course
+                for course in courses
+                if course and course[0].strip() == course_code
+            )
             return course_row[1].strip()
         except StopIteration:
-            self.logger.error(f'Course with code {course_code} not found')
+            self.logger.error(f"Course with code {course_code} not found")
             return None
 
     async def upload(self, files: list, folder_id: str) -> tuple:
         try:
             return await asyncio.gather(
-                *[upload_video(f'{HOME}/vids/{file_name}', folder_id)
-                  for file_name in files]
+                *[
+                    upload_video(f"{HOME}/vids/{file_name}", folder_id)
+                    for file_name in files
+                ]
             )
         finally:
-            self.logger.info(
-                f'Finished uploading videos {files}')
+            self.logger.info(f"Finished uploading videos {files}")
 
             for file_name in files:
-                Merge.remove_file(f'{HOME}/vids/{file_name}')
+                Merge.remove_file(f"{HOME}/vids/{file_name}")
 
     def run(self):
         while True:
@@ -247,19 +279,19 @@ class DaemonApp:
             time.sleep(1)
 
     @staticmethod
-    def create_logger(mode='INFO'):
-        logs = {'INFO': logging.INFO,
-                'DEBUG': logging.DEBUG}
+    def create_logger(mode="INFO"):
+        logs = {"INFO": logging.INFO, "DEBUG": logging.DEBUG}
 
-        logger = logging.getLogger('merger_logger')
+        logger = logging.getLogger("merger_logger")
         logger.setLevel(logs[mode])
 
         handler = logging.StreamHandler()
         handler.setLevel(logs[mode])
 
         formatter = logging.Formatter(
-            '%(levelname)-8s  %(asctime)s    %(message)s',
-            datefmt='%d-%m-%Y %I:%M:%S %p')
+            "%(levelname)-8s  %(asctime)s    %(message)s",
+            datefmt="%d-%m-%Y %I:%M:%S %p",
+        )
 
         handler.setFormatter(formatter)
 
--- merger/core/db/models.py
+++ merger/core/db/models.py
@@ -1,41 +1,72 @@
 import os
+from datetime import datetime
 
-from sqlalchemy import Column, String, Integer, Boolean, create_engine, ForeignKey
+from sqlalchemy import (
+    Column,
+    String,
+    Integer,
+    Boolean,
+    create_engine,
+    ForeignKey,
+    DateTime,
+)
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.expression import func
 from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
-engine = create_engine(os.environ.get('SQLALCHEMY_DATABASE_URI'))
+engine = create_engine(os.environ.get("SQLALCHEMY_DATABASE_URI"))
 Session = sessionmaker(bind=engine)
 
 
-class Record(Base):
-    __tablename__ = 'records'
+class CommonMixin:
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, default=func.now())
+    modified_at = Column(DateTime, default=func.now(), onupdate=func.now())
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    event_name = Column(String(200))
-    room_name = Column(String(200), nullable=False)
+
+class Record(Base, CommonMixin):
+    __tablename__ = "records"
+
     date = Column(String(100), nullable=False)
     start_time = Column(String(100), nullable=False)
     end_time = Column(String(100), nullable=False)
-    user_email = Column(String(100), nullable=False)
-    event_id = Column(String(200))
+    event_name = Column(String(200))
+    event_id = Column(String(200), unique=True)
     drive_file_url = Column(String(200))
 
-    done = Column(Boolean, nullable=False, default=False)
-    processing = Column(Boolean, nullable=False, default=False)
-    error = Column(Boolean, nullable=False, default=False)
+    done = Column(Boolean, default=False)
+    processing = Column(Boolean, default=False)
+    error = Column(Boolean, default=False)
+
+    room_id = Column(Integer, ForeignKey("rooms.id"))
+    room = relationship("Room", back_populates="records")
+    users = relationship("UserRecord", back_populates="record")
 
 
-class Room(Base):
-    __tablename__ = 'rooms'
+class User(Base, CommonMixin):
+    __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False)
-    tracking_state = Column(Boolean, default=False)
-    sources = relationship('Source', backref='room', lazy=False)
+    email = Column(String(120), unique=True, nullable=False)
+    password = Column(String(255), default=None)
+    role = Column(String(50), default="user")
+    email_verified = Column(Boolean, default=False)
+    access = Column(Boolean, default=False)
+    api_key = Column(String(255), unique=True)
+    last_login = Column(DateTime, default=datetime.utcnow)
+
+    records = relationship("UserRecord", back_populates="user")
+
+
+class Room(Base, CommonMixin):
+    __tablename__ = "rooms"
+
+    name = Column(String(100), nullable=False, unique=True)
+    ruz_id = Column(Integer)
+
     drive = Column(String(200))
     calendar = Column(String(200))
+    stream_url = Column(String(300))
 
     sound_source = Column(String(100))
     main_source = Column(String(100))
@@ -43,17 +74,22 @@ class Room(Base):
     screen_source = Column(String(100))
 
     auto_control = Column(Boolean, default=True)
+    tracking_state = Column(Boolean, default=False)
+
+    records = relationship("Record", back_populates="room")
+    sources = relationship("Source", backref="room", lazy=False)
+    channel = relationship("Channel", backref="room", uselist=False)
 
 
 class Source(Base):
-    __tablename__ = 'sources'
+    __tablename__ = "sources"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(100), default='источник')
+    name = Column(String(100), default="источник")
     ip = Column(String(200))
     port = Column(String(200))
-    rtsp = Column(String(200), default='no')
+    rtsp = Column(String(200), default="no")
     audio = Column(String(200))
     merge = Column(String(200))
     tracking = Column(String(200))
-    room_id = Column(Integer, ForeignKey('rooms.id'))
+    room_id = Column(Integer, ForeignKey("rooms.id"))
